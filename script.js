1d6c4d3bae0a1f047fe86dbceb0cae9639d7fce9
hbkim1101 <hbkim94911@gmail.com>
2022-03-27 15:14:53 +0900

    a

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var pf;
 function deviceCheck() {
     document.write(navigator.platform)
     // 디바이스 종류 설정
-    var pcDevice = "win16|win32|win64|mac|macintel";
+    var pcDevice = "win16|win32|win64|mac";
  
     // 접속한 디바이스 환경
     if ( navigator.platform ) {
